f292ba564b51f24eae6bd259d02ba089159c12f2
Wei Sun <wwsun@CNwwsun.local>
2017-07-18 15:46:11 +0800

    Modified: refact for the Notary; Wayde

--- src/Notary.js
+++ src/Notary.js
@@ -11,21 +11,10 @@ class Notary {
 
   judge(answer) {
     this.answer = answer;
-    var result = "";
+    let sameNumberCount = this._countSameNumber();
+    let samePositionCount = this._countSamePosition();
 
-    if(this.answer === this.theCard){
-      result = "4A0B";
-    } else if (this._isUpsideDown()){
-      result = "0A4B";
-    } else if (false == this._hasSameNumber() && false == this._hasSamePosition()) {
-      result = "0A0B";
-    } else {
-      let sameNumberCount = this._countSameNumber();
-      let samePositionCount = this._countSamePosition();
-      result = samePositionCount + 'A' + (sameNumberCount - samePositionCount) + 'B';
-    }
-
-    return result;
+    return samePositionCount + 'A' + (sameNumberCount - samePositionCount) + 'B';
   }
 
   _countSameNumber() {
@@ -49,38 +38,6 @@ class Notary {
 
     return count;
   }
-
-  _hasSameNumber() {
-    var found = false;
-    for(var i = 0; i < this.answer.length; i++){
-      if(-1 !== this.theCard.indexOf(this.answer[i])) {
-        found = true;
-        break;
-      }
-    }
-
-    return found;
-  }
-
-  _hasSamePosition() {
-    var found = false;
-    for(var i = 0; i < this.answer.length; i++){
-      if(this.theCard[i] === this.answer[i]) {
-        found = true;
-        break;
-      }
-    }
-
-    return found;
-  }
-
-  _isUpsideDown() {
-    return this.answer[0] === this.theCard[3] &&
-      this.answer[1] === this.theCard[2] &&
-      this.answer[2] === this.theCard[1] &&
-      this.answer[3] === this.theCard[0]
-  }
-
 
 }
 
